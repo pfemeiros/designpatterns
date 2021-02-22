f917a42e82ba3c13847f5a1240eb00b804f11c00
Pedro Medeiros <pedro.medeiros@redspark.io>
2021-02-22 17:43:48 -0300

    first commit

---
+++ src/test/java/br/com/pfemeiros/designpatterns/DesignpatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pfemeiros.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
